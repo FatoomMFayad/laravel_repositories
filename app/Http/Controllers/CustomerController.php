d13bd5fa85e15c27fc049e21ee42c5ba979e6038
Fatoom Fayad <fatoomfayyad@gmail.com>
2021-04-11 09:32:54 +0300

    format the output

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -11,7 +11,15 @@ class CustomerController extends Controller
     {
         $customers = Customer::orderBy('name')
         ->where('active', 1)->with('user')
-        ->get();
+        ->get()
+        ->map(function ($customer){
+            return [
+                'customer_id' => $customer->id,
+                'name' => $customer->name,
+                'created_by' => $customer->user->email,
+                'last_updated' => $customer->updated_at->diffForHumans(),
+            ];
+        });
         return $customers;
     }
 }
