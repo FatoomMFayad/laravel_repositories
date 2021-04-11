8f0c4b6a845cf006ff7796e9aedf9928166e16d1
Fatoom Fayad <fatoomfayyad@gmail.com>
2021-04-11 12:26:12 +0300

    use interface instead of repository

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -4,12 +4,13 @@ namespace App\Http\Controllers;
 
 use App\Models\Customer;
 use App\Repositories\CustomerRepository;
+use App\Repositories\CustomerRepositoryInterface;
 use Illuminate\Http\Request;
 
 class CustomerController extends Controller
 {
     private $customerRepository;
-    public function __construct(CustomerRepository $customerRepository)
+    public function __construct(CustomerRepositoryInterface $customerRepository)
     {
         $this->customerRepository = $customerRepository;
     }
