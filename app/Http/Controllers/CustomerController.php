ad75728d9ae9852e1099d05c832f1b8fdd60b67f
Fatoom Fayad <fatoomfayyad@gmail.com>
2021-04-11 09:19:40 +0300

    add index function to print all customers

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Customer;
 use Illuminate\Http\Request;
 
 class CustomerController extends Controller
 {
-    //
+    public function index()
+    {
+        $customers = Customer::all();
+        return $customers;
+    }
 }
