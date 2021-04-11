e0331957741a67a191bfb5360b7e521113d6e1a7
Fatoom Fayad <fatoomfayyad@gmail.com>
2021-04-11 10:19:44 +0300

    use customer repository to return the output

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -3,23 +3,19 @@
 namespace App\Http\Controllers;
 
 use App\Models\Customer;
+use App\Repositories\CustomerRepository;
 use Illuminate\Http\Request;
 
 class CustomerController extends Controller
 {
+    private $customerRepository;
+    public function __construct(CustomerRepository $customerRepository)
+    {
+        $this->customerRepository = $customerRepository;
+    }
     public function index()
     {
-        $customers = Customer::orderBy('name')
-        ->where('active', 1)->with('user')
-        ->get()
-        ->map(function ($customer){
-            return [
-                'customer_id' => $customer->id,
-                'name' => $customer->name,
-                'created_by' => $customer->user->email,
-                'last_updated' => $customer->updated_at->diffForHumans(),
-            ];
-        });
+        $customers = $this->customerRepository->all();
         return $customers;
     }
 }
