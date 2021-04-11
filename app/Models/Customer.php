45c0386372c84bb2ef1c632b6f720619b51e0bb4
Fatoom Fayad <fatoomfayyad@gmail.com>
2021-04-11 11:31:17 +0300

    move format method from repository to model

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -9,6 +9,16 @@ class Customer extends Model
 {
     use HasFactory;
 
+    public function format()
+    {
+        return [
+            'customer_id' => $this->id,
+            'name' => $this->name,
+            'created_by' => $this->user->email,
+            'last_updated' => $this->updated_at->diffForHumans(),
+        ];
+    }
+
     public function user()
     {
         return $this->belongsTo(User::class);
