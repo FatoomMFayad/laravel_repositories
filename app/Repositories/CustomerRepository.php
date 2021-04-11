9657f5e7f5e6813e72187ab31a281f7f489d1c5b
Fatoom Fayad <fatoomfayyad@gmail.com>
2021-04-11 10:58:50 +0300

    Refactor formating process to a separated function

--- app/Repositories/CustomerRepository.php
+++ app/Repositories/CustomerRepository.php
@@ -14,21 +14,28 @@ class CustomerRepository
             ->where('active', 1)->with('user')
             ->get()
             ->map(function ($customer){
-                return [
-                    'customer_id' => $customer->id,
-                    'name' => $customer->name,
-                    'created_by' => $customer->user->email,
-                    'last_updated' => $customer->updated_at->diffForHumans(),
-                ];
+                return $this->formatCustomer($customer);
             });
     }
 
     public function findById($customerId)
     {
-        return Customer::where('Id', $customerId)
+        $customer =  Customer::where('Id', $customerId)
             ->where('active', 1)
             ->with('user')
             ->firstOrFail();
+        return $this->formatCustomer($customer);
+    }
+
+    protected function formatCustomer($customer)
+    {
+            return [
+                'customer_id' => $customer->id,
+                'name' => $customer->name,
+                'created_by' => $customer->user->email,
+                'last_updated' => $customer->updated_at->diffForHumans(),
+            ];
+
     }
 
 
